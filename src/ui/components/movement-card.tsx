90a25fa6b2e091dbbddeb226c89f93004875b494
DHVDeveloper <danielhernandezvieiradvlp@gmail.com>
2025-05-29 22:59:26 +0200

    chore: added dark mode colors to movement-card component.

--- src/ui/components/movement-card.tsx
+++ src/ui/components/movement-card.tsx
@@ -15,15 +15,15 @@ export function MovementCard({
 }: MovementsCardProps) {
   const amountColor = isExpense ? "text-red-500" : "text-green-500";
   const iconColor = isExpense
-    ? "text-red-500 bg-red-100"
-    : "text-green-500 bg-green-50";
+    ? "text-red-500  bg-red-100"
+    : "text-green-500 bg-green-100";
   const iconRotation = isExpense ? "rotate-90" : "";
 
   return (
-    <div className="bg-primary-bg-color text-primary-text-color/60 shadow  rounded-2xl w-full h-full px-3 py-2 items-center">
+    <div className="bg-primary-bg-color transition-colors text-primary-text-color/60 shadow  rounded-2xl w-full h-full px-3 py-2 items-center">
       <div className="flex justify-start w-full h-full items-center gap-5">
         <ArrowUpRight
-          className={`h-[45px] w-[45px] ${iconColor} p-2 rounded-lg transform ${iconRotation}`}
+          className={`h-[45px] w-[45px] ${iconColor} dark:bg-[#2c2c2c] p-2 rounded-lg transform ${iconRotation}`}
         />
         <div className="flex flex-col justify-center items-start">
           <p className="text-xs ">{title}</p>
