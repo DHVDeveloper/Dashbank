498ac754a0e1d7fd3a7fa14561ed182ed23b9822
DHVDeveloper <danielhernandezvieiradvlp@gmail.com>
2025-05-31 19:45:52 +0200

    style: ui changes for alert.

--- src/ui/components/alert/atert.tsx
+++ src/ui/components/alert/atert.tsx
@@ -24,8 +24,8 @@ export function Alert({
       className={`min-w-[250px] border-l-4 px-4 py-3 font-bold rounded-sm text-black shadow-md text-sm flex items-center justify-between gap-2
       ${alertStyles[alert.type]}`}
     >
-      <span>{alert.message}</span>
-      <button onClick={onClose} className="text-sm text-[#11111]">
+      <span className="px-1">{alert.message}</span>
+      <button onClick={onClose} className="text-sm text-gray-400 font-bold p-2 cursor-pointer hover:text-gray-900">
        X
       </button>
     </div>
