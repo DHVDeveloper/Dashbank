324b1efcdc1ea0584dc13dd2ba59d8fa736946ad
DHVDeveloper <danielhernandezvieiradvlp@gmail.com>
2025-05-31 19:46:28 +0200

    style: ui fix button component.

--- src/ui/components/form/button.tsx
+++ src/ui/components/form/button.tsx
@@ -9,7 +9,7 @@ export function Button({ children, className = "", ...rest }: ButtonProps) {
     <button
       {...rest}
       className={`inline-flex items-center justify-center py-2 px-4 rounded-md bg-primary-button-bg-color cursor-pointer text-white text-sm
-        disabled:opacity-50 disabled:cursor-not-allowed transition-colors ${className}`}
+        disabled:opacity-50 transition-colors ${className}`}
     >
       {children}
     </button>
