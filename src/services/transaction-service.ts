5e15c71e07a2fafb44f354395ab912a2e5ad3380
DHVDeveloper <danielhernandezvieiradvlp@gmail.com>
2025-06-01 14:44:00 +0200

    chore: added get and remove transaction service.

--- src/services/transaction-service.ts
+++ src/services/transaction-service.ts
@@ -1,15 +1,18 @@
-import type { NewTransaction } from "@/domain/interfaces/transaction/transaction";
-import type { TransactionRequest } from "@/infraestructure/interfaces/transaction.external";
-import { maptransactionToTransactionRequest } from "@/infraestructure/mapper/transaction.external.mapper";
-import { createNewTransaction } from "@/infraestructure/repository/transaction-repository";
-import type { ApiResponse } from "@/types/apiResponse";
+import type { NewTransaction, Transaction } from "@/domain/interfaces/transaction";
+import { mapTransactionListRequestToTransactionList } from "@/domain/mappers/transaction.mapper";
+import type { NewTransactionRequest } from "@/infraestructure/interfaces/transaction.external";
+import { mapNewtransactionToNewTransactionRequest } from "@/infraestructure/mappers/transaction.external.mapper";
+import { createNewTransaction, getTransactionHistoryData, removeTransactionById } from "@/infraestructure/repository/transaction-repository";
+import type { ApiResponse, PaginatedResponse } from "@/types/apiResponse";
 
 export const transactionService = {
+    getTransactionHistory: getTransactionHistory,
     newTransaction: newTransaction,
+    removeTransaction: removeTransaction
 }
 
-async function newTransaction(transaction:NewTransaction): Promise<ApiResponse<TransactionRequest>> {
-    const transactioRequest:TransactionRequest = maptransactionToTransactionRequest(transaction)
+async function newTransaction(transaction:NewTransaction): Promise<ApiResponse<NewTransactionRequest>> {
+    const transactioRequest:NewTransactionRequest = mapNewtransactionToNewTransactionRequest(transaction)
     const response = await createNewTransaction(transactioRequest)
     if(!response.success) {
         return({
@@ -22,3 +25,29 @@ async function newTransaction(transaction:NewTransaction): Promise<ApiResponse<T
     }
 }
 
+async function getTransactionHistory(page:number, size:number): Promise<ApiResponse<PaginatedResponse<Transaction>>> {
+    const response = await getTransactionHistoryData(page, size)
+    if(!response.success || !response.data) {
+        return({
+            success: false,
+            error: response.error
+        })
+    }
+    return {
+        success: response.success,
+        data: {...response.data, data: mapTransactionListRequestToTransactionList(response.data.data)} 
+    }
+}
+
+async function removeTransaction(transactionId:string): Promise<ApiResponse<PaginatedResponse<Transaction>>> {
+    const response = await removeTransactionById(transactionId)
+    if(!response.success) {
+        return({
+            success: false,
+            error: response.error
+        })
+    }
+    return {
+        success: response.success
+    }
+}
