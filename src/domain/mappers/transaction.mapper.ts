ac491d90e95894d694590264e333c88186023839
DHVDeveloper <danielhernandezvieiradvlp@gmail.com>
2025-06-01 14:53:30 +0200

    chore: added domain mappers.

---
+++ src/domain/mappers/transaction.mapper.ts
@@ -0,0 +1,33 @@
+import type { TransactionResponse } from "@/infraestructure/interfaces/transaction.external";
+import type { Transaction } from "../interfaces/transaction";
+import { CurrencyRepository } from "@/infraestructure/interfaces/money.external";
+import { Currency } from "../interfaces/money";
+
+export function mapTransactionRequestToTransaction(transactionResponse:TransactionResponse): Transaction {
+    return {
+        id: transactionResponse.id,
+        date: transactionResponse.date,
+        description: transactionResponse.description,
+        transactionType: transactionResponse.transactionType,
+        money: {
+            amount: transactionResponse.money.amount,
+            currency: mapCurrencyResponseToCurrency(transactionResponse.money.currency)
+        },
+    }
+}
+
+export function mapTransactionListRequestToTransactionList(transactionListResponse:TransactionResponse[]): Transaction[] {
+    return(transactionListResponse.length > 0 ? 
+        transactionListResponse.map(transaction => mapTransactionRequestToTransaction(transaction)) 
+        : [])
+}
+
+
+export function mapCurrencyResponseToCurrency(currency: CurrencyRepository): Currency {
+  switch (currency) {
+    case CurrencyRepository.EUR:
+      return Currency.EUR
+    case CurrencyRepository.KES:
+      return Currency.KES
+  }
+}
