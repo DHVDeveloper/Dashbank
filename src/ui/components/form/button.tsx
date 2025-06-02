b3f4161de34969798dee8c72ef3501996d8ac195
DHVDeveloper <danielhernandezvieiradvlp@gmail.com>
2025-06-02 22:02:54 +0200

    fix: styles in button component.

--- src/ui/components/form/button.tsx
+++ src/ui/components/form/button.tsx
@@ -8,8 +8,8 @@ export function Button({ children, className = "", ...rest }: ButtonProps) {
   return (
     <button
       {...rest}
-      className={`inline-flex items-center justify-center py-2 px-4 rounded-md bg-primary-button-bg-color cursor-pointer text-white text-sm
-        disabled:opacity-50 hover:bg-primary-button-bg-color/80 transition-colors ${className}`}
+      className={`inline-flex items-center justify-center py-2 px-4 rounded-md bg-primary-button-bg-color  text-white text-sm
+        disabled:opacity-50 ${!rest.disabled && 'hover:bg-primary-button-bg-color/30 cursor-pointer'}  transition-colors ${className}`}
     >
       {children}
     </button>
