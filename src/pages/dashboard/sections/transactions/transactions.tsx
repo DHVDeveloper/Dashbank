f9b8f155a1cbf14b5db8ba98f1069e31efcfc6a4
DHVDeveloper <danielhernandezvieiradvlp@gmail.com>
2025-06-02 22:45:32 +0200

    fix: fix responsive in transaction section.

--- src/pages/dashboard/sections/transactions/transactions.tsx
+++ src/pages/dashboard/sections/transactions/transactions.tsx
@@ -1,15 +1,17 @@
 import { TransactionsCreate } from "./sections/transaction-create/transaction-create";
 import { TransactionFilters } from "./sections/transaction-filters/transaction-filters";
 import { TransactionListSection } from "./sections/transaction-list/transaction-list-section";
+import { TransactionRevert } from "./sections/transaction-revert/transaction-revert";
 
 export function Transactions(){
     return(<div className="w-full h-auto">
         <div className="flex flex-col gap-4">
-            <div className="flex justify-between items-center">
+            <div className="flex flex-col md:flex-row justify-between items-center">
                 <h3 className="text-sm text-primary-text-color font-black">TRANSACTIONS</h3>
-                <div className="flex gap-2">
+                <div className="flex justify-end flex-wrap gap-2">
                     <TransactionFilters/>
                     <TransactionsCreate/>
+                    <TransactionRevert/>
                 </div>
             </div>
             <TransactionListSection/>
